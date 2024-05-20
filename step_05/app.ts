33f5322ca7c25f9248c9145e8623feb38dfa243c
Bisma-Shah <165302776+Bisma-Shah@users.noreply.github.com>
2024-05-20 17:41:34 +0500

    Upload files

---
+++ step_05/app.ts
@@ -0,0 +1,8 @@
+/* 5. Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+Then compose your message and store it in a new variable called message. Print your message. */
+
+let famous_Person: string = "Michael Jordan";
+
+let message: string = "I can accept failure, everyone fails at something But I cant accept not trying.";
+
+console.log(`${famous_Person} once said, "${message}"`);
